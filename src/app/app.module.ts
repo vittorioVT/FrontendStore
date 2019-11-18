167a540aba705ca5d72f121a8f01907f09a42cc1
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-11-18 15:50:40 +0200

    Created productsStore, footer, header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsStoreComponent } from './products-store/products-store.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsStoreComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
