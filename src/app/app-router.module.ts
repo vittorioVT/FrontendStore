6c11fa9e4f82f22eb645d27136d08379f1c3377a
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-11-22 18:53:38 +0200

    Added routes for submenu

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -2,12 +2,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 //component
 import { ProductsStoreComponent } from './products-store/products-store.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { DressBoyComponent } from './dress-boy/dress-boy.component';
+import { DressGirlComponent } from './dress-girl/dress-girl.component';
+import { AttributsPlComponent } from './attributs-pl/attributs-pl.component';
+import { AccessorsPlComponent } from './accessors-pl/accessors-pl.component';
+
 import { NgModule } from '@angular/core';
 
 //route
 const routes: Routes = [
   { path: '', component: ProductsStoreComponent },
-  { path: 'productsStore', component: ProductsStoreComponent }
+  { path: 'productsStore', component: ProductsStoreComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'dressboy', component: DressBoyComponent },
+  { path: 'dressgirl', component: DressGirlComponent },
+  { path: 'attributs', component: AttributsPlComponent },
+  { path: 'accessors', component: AccessorsPlComponent },
 
 ]
 
