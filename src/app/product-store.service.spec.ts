a1a363377d400f1d12db303ca27b3780d0af9ad3
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-04-28 11:34:16 +0300

    Added httpClient for spec productStoreService

--- src/app/product-store.service.spec.ts
+++ src/app/product-store.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ProductStoreService } from './product-store.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ProductStoreService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  xit('should be created', () => {
-    const service: ProductStoreService = TestBed.get(ProductStoreService);
+  it('should be created', () => {
+    const service: ProductStoreService = new ProductStoreService(new HttpClient(null));
     expect(service).toBeTruthy();
   });
 });
