5fc1cf27db54095fe672bd463470780c4760509c
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-04-28 12:29:20 +0300

    Test of have routerOutlet dynamic derective

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,42 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [RouterTestingModule],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+     fixture = TestBed.createComponent(AppComponent);
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'FrontendStore'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('FrontendStore');
   });
+
+  it('should have router-outlet directive', () => {
+    const de = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(de).not.toBeNull();
+  })
+
 
   //it('should render title', () => {
   //  const fixture = TestBed.createComponent(AppComponent);
