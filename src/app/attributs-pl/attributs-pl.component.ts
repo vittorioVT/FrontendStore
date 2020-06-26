a372ee5de39047e5947ebd7eb28284c0b1d91363
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-06-27 00:02:17 +0300

    Created functionality for cart and for adding products to cart

--- src/app/attributs-pl/attributs-pl.component.ts
+++ src/app/attributs-pl/attributs-pl.component.ts
@@ -10,8 +10,15 @@ import { MatCardModule, MatCardContent, MatCard } from '@angular/material';
   styleUrls: ['./attributs-pl.component.css']
 })
 export class AttributsPlComponent implements OnInit {
+   
+  attribut: ProductElements[] = [];
+  cartSelect: ProductElements[] = [];
 
-  attribut: ProductElements[];
+  isOrderContent = false;
+  price: number = 0;
+  quantity: number = 1;
+  total: number = 0;
+  totalSum: number = 0;
 
 
   constructor(private rubricService: RubricService) { }
@@ -21,5 +28,32 @@ export class AttributsPlComponent implements OnInit {
       this.attribut = data.filter(x => x.Comment.includes('attributs'));
     })
   }
+  addCart(id: number) {
+    let result = this.attribut.find(x => x.Id === id);
+    this.total += 1;
+    let index: number = 0;
+
+    if (this.cartSelect.length === 0) {
+      this.cartSelect.push(result);
+      this.cartSelect[this.cartSelect.length - 1].Quantity += 1;
+      this.totalSum += result.Price;
+
+    } else if (!this.cartSelect.includes(result)) {
+      this.cartSelect.push(result);
+      this.cartSelect[this.cartSelect.length - 1].Quantity += 1;
+      this.totalSum += result.Price;
+
+    } else if (this.cartSelect.includes(result)) {
+      index = this.cartSelect.findIndex(x => x.Id === result.Id);
+      this.cartSelect[index].Quantity += 1;
+      this.totalSum += result.Price;
+    }
+    //  console.log(this.cartSelect);
+    result = null;
+    this.isOrderContent = true;
+  }
+
+
+
 
 }
